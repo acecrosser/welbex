51c2f66d2f9f60370a9e0cd22d747cd068631a01
acecrosser <acecrosser@yandex.ru>
2021-05-14 17:38:08 +0300

    mf

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request
+from flask import render_template
 from confg import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -10,7 +11,39 @@ migrate = Migrate(app, db)
 
 from model import SinglePageTable
 
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index_page():
-    return 'Hello beach'
+
+    filter_item = request.form.get('text_input')
+    column = request.form.get('column')
+    param = request.form.get('param')
+    page = request.args.get('page', 1, type=int)
+
+    if param == 'ilike':
+        table = SinglePageTable.query.filter(
+            getattr(SinglePageTable, f'{column}').ilike(f'{filter_item}')
+        ).paginate(page, 10, False)
+        return render_template('index.html', table=table)
+
+    elif param == '==':
+        table = SinglePageTable.query.filter(
+            getattr(SinglePageTable, f'{column}') == (f'{filter_item}')
+        ).paginate(page, 10, False)
+        return render_template('index.html', table=table)
+
+    elif param == '>':
+        table = SinglePageTable.query.filter(
+            getattr(SinglePageTable, f'{column}') > (f'{filter_item}')
+        ).paginate(page, 10, False)
+        return render_template('index.html', table=table)
+
+    elif param == '<':
+        table = SinglePageTable.query.filter(
+            getattr(SinglePageTable, f'{column}') < (f'{filter_item}')
+        ).paginate(page, 10, False)
+        return render_template('index.html', table=table)
+
+    else:
+        page = request.args.get('page', 1, type=int)
+        table = SinglePageTable.query.paginate(page, 10, False)
+        return render_template('index.html', table=table)
