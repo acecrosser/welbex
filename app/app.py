335b3067d8961ca358086283f4375a57726b7d8e
acecrosser <acecrosser@yandex.ru>
2021-05-15 23:10:54 +0300

    omg this funy js

--- app/app.py
+++ app/app.py
@@ -11,39 +11,44 @@ migrate = Migrate(app, db)
 
 from model import SinglePageTable
 
-@app.route('/', methods=['GET', 'POST'])
-def index_page():
 
-    filter_item = request.form.get('text_input')
-    column = request.form.get('column')
-    param = request.form.get('param')
-    page = request.args.get('page', 1, type=int)
+@app.route('/')
+def index():
+
+    table = SinglePageTable.query.all()
+    return render_template('index.html', table=table)
+
+
+@app.route('/table')
+def tabel_page():
+    
+    column = request.args.get('column')
+    param = request.args.get('param')
+    filter_item = request.args.get('text_input')
 
     if param == 'ilike':
         table = SinglePageTable.query.filter(
-            getattr(SinglePageTable, f'{column}').ilike(f'{filter_item}')
-        ).paginate(page, 10, False)
-        return render_template('index.html', table=table)
+            getattr(SinglePageTable, f'{column}').ilike(f'%{filter_item}%')
+        ).order_by(getattr(SinglePageTable, f'{column}'))
+        return render_template('table.html', table=table)
 
     elif param == '==':
         table = SinglePageTable.query.filter(
             getattr(SinglePageTable, f'{column}') == (f'{filter_item}')
-        ).paginate(page, 10, False)
-        return render_template('index.html', table=table)
+        ).order_by(getattr(SinglePageTable, f'{column}'))
+        return render_template('table.html', table=table)
 
     elif param == '>':
         table = SinglePageTable.query.filter(
             getattr(SinglePageTable, f'{column}') > (f'{filter_item}')
-        ).paginate(page, 10, False)
-        return render_template('index.html', table=table)
+        ).order_by(getattr(SinglePageTable, f'{column}'))
+        return render_template('table.html', table=table)
 
     elif param == '<':
         table = SinglePageTable.query.filter(
             getattr(SinglePageTable, f'{column}') < (f'{filter_item}')
-        ).paginate(page, 10, False)
-        return render_template('index.html', table=table)
+        ).order_by(getattr(SinglePageTable, f'{column}'))
+        return render_template('table.html', table=table)
 
-    else:
-        page = request.args.get('page', 1, type=int)
-        table = SinglePageTable.query.paginate(page, 10, False)
-        return render_template('index.html', table=table)
+    table = SinglePageTable.query.all()
+    return render_template('table.html', table=table)
--- app/confg.py
+++ app/confg.py
@@ -1,6 +1,4 @@
 
-
-class Configuration:
-    DEBUG = True
+class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:OSXC715MICHElle@localhost:5432/db_welbex'
