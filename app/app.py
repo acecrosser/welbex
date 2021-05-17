308527fabb190b5ae971359d2981f6e86870c2b1
acecrosser <acecrosser@yandex.ru>
2021-05-17 15:04:17 +0300

    fixed import for linux

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 from flask import render_template
-from .confg import Configuration
+from confg import Configuration
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
